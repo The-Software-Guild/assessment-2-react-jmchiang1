c418b5512c19f55c5a236c433b306eeef12c3794
Jonathan Chiang <jonathanchiang7@gmail.com>
2021-12-18 11:55:56 -0500

    router.delete works

--- Section-Two/crud/server/routes/Routes.js
+++ Section-Two/crud/server/routes/Routes.js
@@ -2,17 +2,14 @@ const express = require("express");
 const router = express.Router();
 let todos = require("../dummyData");
 const { v4: uuidv4 } = require("uuid");
-const { resolveSoa } = require("dns");
 
 //get all todos - WORKING
 router.get("/", function (req, res, next) {
     try {
         res.status(200).send(todos);
     } catch (err){
-        res.status(400).json({
-            message: 'get all todos error occured',
-            err,
-        })
+        res.status(400).send("Invalid GET all request");
+        next(err);
     }
 });
 
@@ -25,30 +22,31 @@ router.get("/:id", (req, res, next) => {
       res.status(200).send(findTodo);
       console.log("GET SINGLE ITEM SUCCESSFUL");
   } catch (err){
-      res.status(400).json({
-          message: 'get sinlge item error occured',
-        })
-        next(err)
+      res.status(400).send("Invalid GET by ID request");
+      next(err);
   }
 });
 
 //update existing item by id - WORKS
 router.patch("/:id", (req, res, next) => {
   let id  = req.params.id;
-//   const { Name, Task, Easy, Count, Day } = req.body;
-  const item = todos.find((item) => item.id === parseInt(id));
-  if (!item) {
-      return res.status(404).send("Item with ID doesn't exist")
+  try {
+      const item = todos.find((item) => item.id === parseInt(id));
+      if (!item) {
+          return res.status(404).send("Item with ID doesn't exist");
+      }
+      item.Name = req.body.Name;
+      item.Task = req.body.Task;
+      item.Easy = req.body.Easy;
+      item.Count = req.body.Count;
+      item.Day = req.body.Day;
+      res
+        .status(200).send(item);
+        // .redirect("/");
+  } catch (err){
+      res.status(404).send('Invalid Item update');
+      next(err);
   }
-  item.Name = req.body.Name;
-  item.Task = req.body.Task;
-  item.Easy = req.body.Easy;
-  item.Count = req.body.Count;
-  item.Day = req.body.Day;
-  res
-    .status(200)
-      .send(item);
-    // .redirect("/");
 });
 
 //create new item - WORKS
@@ -59,18 +57,25 @@ router.post("/", (req, res, next) => {
       todos.push({ ...newItem, id: uuidv4() });
       res.status(200)
       .send(newItem)
-    //   .redirect("/")
+    //   .redirect("/todos")
   } catch (err) {
-      res.status(404).send("Invalid creation")
+      res.status(404).send("Invalid creation");
+      next(err);
   }
 });
 
 //delete single item - 
 router.delete("/:id", (req, res, next) => {
-  let { id } = req.params;
-  todos = todos.filter((item) => item.id !== id);
-  res.status(200).send(todos);
-  // .redirect("/")
+  let id = req.params.id;
+  try {
+      todos = todos.filter((item) => item.id !== parseInt(id));
+      res.status(200)
+    //   .send(todos);
+      .redirect("/todos")
+  } catch (err) {
+      res.send(404).send('Invalid Deletion');
+      next(err);
+  }
 });
 
 module.exports = router;
